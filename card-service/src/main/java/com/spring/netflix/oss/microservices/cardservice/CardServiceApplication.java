fe63fb8763aac51b50a33e7d26329893d9ca1982
JITHESHKUMAR THEKKEVEETTIL <jithus@JITHESHKUMARs-MacBook-Pro.local>
2018-10-03 16:16:20 -0500

    card-service

---
+++ card-service/src/main/java/com/spring/netflix/oss/microservices/cardservice/CardServiceApplication.java
@@ -0,0 +1,14 @@
+package com.spring.netflix.oss.microservices.cardservice;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardServiceApplication.class, args);
+	}
+}
