b7457de8fd137bbf9d5cd5dc877682b896812b68
JITHESHKUMAR THEKKEVEETTIL <jithus@JITHESHKUMARs-MacBook-Pro.local>
2018-10-03 15:41:36 -0500

    Added import

--- edge-server/src/main/java/com/spring/netflix/oss/microservices/edgeserver/EdgeServerApplication.java
+++ edge-server/src/main/java/com/spring/netflix/oss/microservices/edgeserver/EdgeServerApplication.java
@@ -1,10 +1,12 @@
 package com.spring.netflix.oss.microservices.edgeserver;
 
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulServer
 public class EdgeServerApplication {
 
